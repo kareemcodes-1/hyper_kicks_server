e87d47a6f4d3be1e666425fe79363342056488c6
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-31 09:12:40 +0100

    server changes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { getAllOrders, getAllOrdersByMonth, getAllSalesByMonth, getUserOrders } from "../controllers/orderController.js";
-import { adminMiddleware } from "../middleware/protectRoute.js";
+import { adminMiddleware, protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get('/', getAllOrders);
-router.get('/user/:userId', getUserOrders);
+router.get('/user/:userId', protectRoute, getUserOrders);
 router.get('/sales/month', adminMiddleware, getAllSalesByMonth);
 router.get('/month', adminMiddleware, getAllOrdersByMonth);
 
